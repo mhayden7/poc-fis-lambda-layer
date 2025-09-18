04e75bc6f24b0649252e12f0c9efe9a439353e3a
mhayden7 <mhayden7@gmail.com>
2025-09-18 15:32:29 -0400

    adding dotnet lambda

---
+++ lambdas/dotnet/src/dotnet/Function.cs
@@ -0,0 +1,26 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace dotnet;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public string FunctionHandler(Args input, ILambdaContext context)
+    {
+        return "Hello from fis_dotnet Lambda!";
+    }
+}
+
+public class Args
+{
+    public string input{get; set;}
+}
